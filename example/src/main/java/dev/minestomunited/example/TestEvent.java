f9fd3c1f17fbfbc78ad637284b85142a2cafddba
Webhead1104 <webhead1104@hotmail.com>
2026-06-09 16:26:15 -0500

    chore: remove MinestomEvent annotation

--- example/src/main/java/dev/minestomunited/example/TestEvent.java
+++ example/src/main/java/dev/minestomunited/example/TestEvent.java
@@ -1,9 +1,7 @@
 package dev.minestomunited.example;
 
-import dev.minestomunited.minestomevents.MinestomEvent;
 import net.minestom.server.event.Event;
 
-@MinestomEvent
 public class TestEvent implements Event {
 
 }
